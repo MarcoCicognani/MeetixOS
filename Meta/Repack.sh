22e4d0b854b6420b75361cab9a06bbfaeca32678
Marco Cicognani <marco.cicognani@meetixos.org>
2021-09-09 11:14:07 +0200

    Renamed libraries with Lib prepended. Stubbed Repack.sh script

--- Meta/Repack.sh
+++ Meta/Repack.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 
+for CMD_ARG in "$@"; do
+    case $CMD_ARG in
+        --out-dir=*)
+            OUT_DIR=${CMD_ARG#*=}
+            if [ ! -d "$OUT_DIR" ]; then
+                echo "out-dir seems to be un-existing, be sure to build for this target"
+                exit 1
+            fi
+            ;;
+        *)
+            echo "Bootable image build script"
+            echo "USAGE:"
+            echo "    Repack.sh --out-dir=/Path/To/OutputDir"
+            exit 0
+            ;;
+    esac
+done
