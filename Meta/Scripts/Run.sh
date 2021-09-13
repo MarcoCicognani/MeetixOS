7bc52e8ba09cfd35b52647c8dd1ebd81c7b4dc54
Marco Cicognani <marco.cicognani@meetixos.org>
2021-09-13 10:39:27 +0200

    Implemented Run.sh script

--- Meta/Scripts/Run.sh
+++ Meta/Scripts/Run.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
 
+for CMD_ARG in "$@"; do
+    case $CMD_ARG in
+        --iso-dir=*)
+            ISO_DIR=${CMD_ARG#*=}
+            if [ ! -d "$ISO_DIR" ]; then
+                echo "$$ISO_DIR seems to be un-existing, be sure to build for this target"
+                exit 1
+            fi
+            ;;
+        *)
+            echo "Run MeetixOS ISO image script"
+            echo "USAGE:"
+            echo "    Run.sh --iso-dir=/Path/To/OutputDir"
+            exit 0
+            ;;
+    esac
+done
+
+# run the iso image
+qemu-system-i386 -enable-kvm -m 512M -serial stdio -cdrom "$ISO_DIR/MeetixOS.iso" || exit 1
