891a6ca54d7ca7b02f1ec9b1cf103b05888e65a8
Marco Cicognani <marco.cicognani@meetixos.org>
2021-10-08 09:57:29 +0200

    Userspace/Commands/Idle: Rewritten in C++ Style

---
+++ Userspace/Commands/Idle/Idle.cc
@@ -0,0 +1,24 @@
+/**
+ * @brief
+ * This file is part of the MeetiX Operating System.
+ * Copyright (c) 2017-2021, Marco Cicognani (marco.cicognani@meetixos.org)
+ *
+ * @developers
+ * Marco Cicognani (marco.cicognani@meetixos.org)
+ *
+ * @license
+ * GNU General Public License version 3
+ */
+
+#include <csignal>
+
+int main() {
+    /* ignore interruption signals */
+    std::signal(SIGINT, SIG_IGN);
+
+    /* cause interruptions */
+    while ( true ) {
+        asm volatile("hlt");
+    }
+    __builtin_unreachable();
+}
