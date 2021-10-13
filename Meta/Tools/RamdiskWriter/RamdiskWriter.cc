6db53dec2a5f7c12abbcb1af3e2a1d6d9008dea4
Marco Cicognani <marco.cicognani@meetixos.org>
2021-10-13 12:15:01 +0200

    Meta/Tools/RamdiskWriter: Changed output style and C++ extensions

--- Meta/Tools/RamdiskWriter/RamdiskWriter.cc
+++ Meta/Tools/RamdiskWriter/RamdiskWriter.cc
@@ -1,28 +1,21 @@
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *                                                                           *
- *  Ghost, a micro-kernel based operating system for the x86 architecture    *
- *  Copyright (C) 2015, Max Schlüssel <lokoxe@gmail.com>                     *
- *                                                                           *
- *  This program is free software: you can redistribute it and/or modify     *
- *  it under the terms of the GNU General Public License as published by     *
- *  the Free Software Foundation, either version 3 of the License, or        *
- *  (at your option) any later version.                                      *
- *                                                                           *
- *  This program is distributed in the hope that it will be useful,          *
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
- *  GNU General Public License for more details.                             *
- *                                                                           *
- *  You should have received a copy of the GNU General Public License        *
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.    *
- *                                                                           *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/**
+ * @brief
+ * This file is part of the MeetiX Operating System.
+ * Copyright (c) 2017-2021, Marco Cicognani (marco.cicognani@meetixos.org)
+ *
+ * @developers
+ * Marco Cicognani (marco.cicognani@meetixos.org)
+ *
+ * @license
+ * GNU General Public License version 3
+ */
 
-#include "RamdiskWriter.hpp"
+#include "RamdiskWriter.hh"
 
 #include <algorithm>
 #include <cstring>
 #include <filesystem>
+#include <iomanip>
 #include <iostream>
 
 /**
@@ -152,8 +145,8 @@ void RamdiskWriter::write_recursive(const std::string& base_path,
     auto       buffer_ptr  = new char[BUFFER_SIZE];
     auto       entry_id    = m_next_id++;
 
-    std::cout << "-- Packing: " << (is_file ? "File" : "Dir ") << ": '" << path
-              << "' with ID: " << entry_id << std::endl;
+    std::cout << "-- Packing: " << (is_file ? "File" : "Dir ") << " with ID: " << std::setfill('0')
+              << std::setw(3) << entry_id << ": '" << path << std::endl;
 
     /* skip root entry */
     if ( entry_id > 0 ) {
---
+++ Meta/Tools/RamdiskWriter/RamdiskWriter.hh
@@ -0,0 +1,47 @@
+/**
+ * @brief
+ * This file is part of the MeetiX Operating System.
+ * Copyright (c) 2017-2021, Marco Cicognani (marco.cicognani@meetixos.org)
+ *
+ * @developers
+ * Marco Cicognani (marco.cicognani@meetixos.org)
+ *
+ * @license
+ * GNU General Public License version 3
+ */
+
+#pragma once
+
+#include <cstdint>
+#include <fstream>
+#include <string>
+#include <vector>
+
+/**
+ * @brief Creates a custom format ramdisk with the MeetiX kernel is able to read and write
+ */
+class RamdiskWriter {
+public:
+    RamdiskWriter() = default;
+
+    /**
+     * @brief Recursively creates the ramdisk image packing the files and the directories found into
+     * source_path into a file referenced by target_path
+     * @param source_path The source directory to pack
+     * @param target_path The target file where the ramdisk is generated
+     */
+    void create(const std::string& source_path, const std::string& target_path);
+
+private:
+    void write_recursive(const std::string& base_path,
+                         const std::string& path,
+                         const std::string& name,
+                         uint32_t           content_length,
+                         uint32_t           parent_id,
+                         bool               is_file);
+
+private:
+    int                      m_next_id{ 0 };
+    std::ofstream            m_out_file{};
+    std::vector<std::string> m_ignores{ std::string{ "*.keep" } };
+};
