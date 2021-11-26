69cc6c70d752057b1403bcbb81859cd88d9d7745
Marco Cicognani <marco.cicognani@meetixos.org>
2021-11-26 16:43:01 +0100

    Repository: Added new sub commands to run `MeetixOS` in Debug mode

--- Meta/Scripts/Run.sh
+++ Meta/Scripts/Run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+QEMU_DEBUG=0
+QEMU_SMP_CORES=4
+QEMU_MEMORY=512
+QEMU_SERIAL=stdio
+
+QEMU_COMMAND=qemu-system-i386
+
 for CMD_ARG in "$@"; do
     case $CMD_ARG in
         --iso-dir=*)
@@ -9,14 +16,42 @@ for CMD_ARG in "$@"; do
                 exit 1
             fi
             ;;
+        --debug)
+            QEMU_DEBUG=1
+            ;;
+        --smp=*)
+            QEMU_SMP_CORES=${CMD_ARG#*=}
+            ;;
+        --mem=*)
+            QEMU_MEMORY=${CMD_ARG#*=}
+            ;;
+        --serial=*)
+            QEMU_SERIAL=${CMD_ARG#*=}
+            ;;
         *)
             echo "Run MeetixOS ISO image script"
             echo "USAGE:"
-            echo "    Run.sh --iso-dir=/Path/To/OutputDir"
+            echo "    Run.sh <--iso-dir=/Path/To/OutputDir> [--debug] [--smp=N] [--mem=N] [--serial=/Path/To/Output]"
             exit 0
             ;;
     esac
 done
 
-# run the iso image
-qemu-system-i386 -enable-kvm -m 512M -cpu host -smp 4 -serial stdio -cdrom "$ISO_DIR/MeetixOS.iso" || exit 1
+QEMU_OPTIONS="-m ${QEMU_MEMORY}M -serial $QEMU_SERIAL -cdrom $ISO_DIR/MeetixOS.iso"
+
+if [[ $QEMU_DEBUG -eq 0 ]]; then
+    QEMU_OPTIONS="-enable-kvm $QEMU_OPTIONS"
+else
+    QEMU_OPTIONS="$QEMU_OPTIONS -s -S"
+fi
+
+if [[ $QEMU_SMP_CORES -ne 0 ]]; then
+    if [[ $QEMU_DEBUG -eq 0 ]]; then
+        QEMU_OPTIONS="$QEMU_OPTIONS -cpu host"
+    fi
+
+    QEMU_OPTIONS=" $QEMU_OPTIONS -smp $QEMU_SMP_CORES"
+fi
+
+# run the OS
+$QEMU_COMMAND $QEMU_OPTIONS || exit 1
