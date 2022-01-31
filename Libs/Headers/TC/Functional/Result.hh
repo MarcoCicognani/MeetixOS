8d3552de5df76a936c7fbf38b70bc179884fe95e
Marco Cicognani <marco.cicognani@meetixos.org>
2022-01-31 16:22:12 +0100

    LibTC: Implemented `Result<T&, E>`

--- Libs/Headers/TC/Functional/Result.hh
+++ Libs/Headers/TC/Functional/Result.hh
@@ -113,6 +113,53 @@ private:
 };
 
 template<typename T, typename E>
+class Result<T&, E> {
+public:
+
+public:
+    /**
+     * @brief Constructors
+     */
+    Result(T const& value);
+    Result(T&& value);
+    Result(E const& error);
+    Result(E&& error);
+
+    Result()                  = delete;
+    Result(Result const&)     = default;
+    Result(Result&&) noexcept = default;
+    ~Result()                 = default;
+
+    /**
+     * @brief Returns the reference to the result value
+     */
+    T&       value();
+    T const& value() const;
+
+    /**
+     * @brief Returns the reference to the result error
+     */
+    E&       error();
+    E const& error() const;
+
+    /**
+     * @brief Unwraps the value or the error if exists
+     */
+    [[nodiscard]] T& unwrap_value();
+    [[nodiscard]] E  unwrap_error();
+
+    /**
+     * @brief Returns whether this result contains a value or an error
+     */
+    [[nodiscard]] bool is_value() const;
+    [[nodiscard]] bool is_error() const;
+
+private:
+    Option<T&> m_value_option{};
+    Option<E>  m_error_option{};
+};
+
+template<typename T, typename E>
 Result<T, E>::Result(T const& value)
     : m_value_option{ value } {
 }
@@ -219,6 +266,66 @@ bool Result<void, E>::is_value() const {
 
 template<typename E>
 bool Result<void, E>::is_error() const {
+    return m_error_option.is_present();
+}
+
+template<typename T, typename E>
+Result<T&, E>::Result(T const& value)
+    : m_value_option{ value } {
+}
+
+template<typename T, typename E>
+Result<T&, E>::Result(T&& value)
+    : m_value_option{ std::move(value) } {
+}
+
+template<typename T, typename E>
+Result<T&, E>::Result(E const& error)
+    : m_error_option{ error } {
+}
+
+template<typename T, typename E>
+Result<T&, E>::Result(E&& error)
+    : m_value_option{ std::move(error) } {
+}
+
+template<typename T, typename E>
+T& Result<T&, E>::value() {
+    return m_value_option.value();
+}
+
+template<typename T, typename E>
+T const& Result<T&, E>::value() const {
+    return m_value_option.value();
+}
+
+template<typename T, typename E>
+E& Result<T&, E>::error() {
+    return m_error_option.value();
+}
+
+template<typename T, typename E>
+E const& Result<T&, E>::error() const {
+    return m_error_option.value();
+}
+
+template<typename T, typename E>
+T& Result<T&, E>::unwrap_value() {
+    return m_value_option.unwrap();
+}
+
+template<typename T, typename E>
+E Result<T&, E>::unwrap_error() {
+    return m_error_option.unwrap();
+}
+
+template<typename T, typename E>
+bool Result<T&, E>::is_value() const {
+    return m_value_option.is_present();
+}
+
+template<typename T, typename E>
+bool Result<T&, E>::is_error() const {
     return m_error_option.is_present();
 }
 
