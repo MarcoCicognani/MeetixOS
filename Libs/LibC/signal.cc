e703e41e102309cea10db2453c6c0a632e1ec05e
Marco Cicognani <marco.cicognani@meetixos.org>
2022-02-21 15:55:25 +0100

    LibC: Collapse signal into single `.cc` file

--- Libs/LibC/signal.cc
+++ Libs/LibC/signal.cc
@@ -10,11 +10,31 @@
  * GNU General Public License version 3
  */
 
-#include <Api.h>
+#include <Api/User.h>
 #include <errno.h>
 #include <signal.h>
+#include <stdlib.h>
 
-extern "C" int raise(int sig) {
+extern "C" {
+
+void sig_handler_SIG_INT(int) {
+    exit(0);
+}
+
+void sig_handler_SIG_IGN(int) {
+}
+
+sig_handler_t signal(int sig, sig_handler_t sig_handler) {
+    if ( sig_handler == SIG_DFL ) {
+        if ( sig == SIGINT )
+            sig_handler = sig_handler_SIG_INT;
+    } else if ( sig_handler == SIG_IGN )
+        sig_handler = sig_handler_SIG_IGN;
+
+    return reinterpret_cast<sig_handler_t>(s_register_signal_handler(sig, reinterpret_cast<void*>(sig_handler)));
+}
+
+int raise(int sig) {
     auto raise_status = s_raise_signal(s_get_tid(), sig);
     if ( raise_status == RAISE_SIGNAL_STATUS_SUCCESSFUL ) {
         return 0;
@@ -25,4 +45,5 @@ extern "C" int raise(int sig) {
         errno = ENOENT;
         return -1;
     }
+}
 }
