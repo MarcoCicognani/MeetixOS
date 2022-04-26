20c95390e2414e91e8b805cc39b174409f1f513b
MarcoCicognani <marco.cicognani@meetixos.org>
2022-04-26 21:59:12 +0200

    Meta: Implemented logic to run MeetixOS through QEMU on Windows

--- Meta/Run.sh
+++ Meta/Run.sh
@@ -5,7 +5,10 @@ QEMU_SMP_CORES=1
 QEMU_MEMORY=512
 QEMU_SERIAL=stdio
 
-QEMU_COMMAND=qemu-system-i386
+is_wsl() {
+    command -v wslpath >/dev/null
+    return $?
+}
 
 for CMD_ARG in "$@"; do
     case $CMD_ARG in
@@ -14,6 +17,10 @@ for CMD_ARG in "$@"; do
             if [ ! -d "$ISO_DIR" ]; then
                 echo "$$ISO_DIR seems to be un-existing, be sure to build for this target"
                 exit 1
+            fi
+
+            if is_wsl; then
+                ISO_DIR="$(wslpath -w "$ISO_DIR")"
             fi
             ;;
         --debug)
@@ -37,21 +44,33 @@ for CMD_ARG in "$@"; do
     esac
 done
 
+if is_wsl; then
+    QEMU_COMMAND="$(wslpath "C:\\Program Files\\qemu")/qemu-system-i386.exe"
+else
+    QEMU_COMMAND=qemu-system-i386
+fi
+
 QEMU_OPTIONS="-m ${QEMU_MEMORY}M -serial $QEMU_SERIAL -cdrom $ISO_DIR/MeetixOS.iso"
 
 if [[ $QEMU_DEBUG -eq 0 ]]; then
-    QEMU_OPTIONS="-enable-kvm $QEMU_OPTIONS"
+    if is_wsl; then
+        QEMU_OPTIONS="-accel whpx,kernel-irqchip=off -accel tcg $QEMU_OPTIONS"
+    else
+        QEMU_OPTIONS="-enable-kvm $QEMU_OPTIONS"
+    fi
 else
     QEMU_OPTIONS="$QEMU_OPTIONS -s -S"
 fi
 
 if [[ $QEMU_SMP_CORES -ne 0 ]]; then
     if [[ $QEMU_DEBUG -eq 0 ]]; then
-        QEMU_OPTIONS="$QEMU_OPTIONS -cpu host"
+        if ! is_wsl; then
+            QEMU_OPTIONS="$QEMU_OPTIONS -cpu host"
+        fi
     fi
 
-    QEMU_OPTIONS=" $QEMU_OPTIONS -smp $QEMU_SMP_CORES"
+    QEMU_OPTIONS="$QEMU_OPTIONS -smp $QEMU_SMP_CORES"
 fi
 
 # run the OS
-$QEMU_COMMAND $QEMU_OPTIONS || exit 1
+"$QEMU_COMMAND" $QEMU_OPTIONS || exit 1
